aa32d68b8a55512be447a1dbd3914bace3e0136a
Andrzej Wójtowicz <andre@amu.edu.pl>
2016-10-18 18:23:23 +0200

    added function to create a barplot

--- s7-compare-results.R
+++ s7-compare-results.R
@@ -9,107 +9,111 @@ flog.info("Step 7: compare results")
 
 # ---- classifiers-original ----
 
-classifiers.original.show.plot = function()
+palette.fill = c("Original classifiers"        = "#0078B8", # color blind friendly palette
+                 "Uncertaintified classifiers" = "#F26522",
+                 "Imputation"                  = "#00A574",
+                 "Aggregation strategy"        = "#F3E737")
+
+palette.shapes = c("Original classifiers"        = 21,
+                   "Uncertaintified classifiers" = 24,
+                   "Imputation"                  = 22,
+                   "Aggregation strategy"        = 25)
+
+get.barplot = function(data)
 {
-    ds.names = expand.grid(DATASETS.NAMES, c(CLASSIFIERS.BASELINE, CLASSIFIERS.LIST),
-                           stringsAsFactors = FALSE)
+    # data: Model | Value.min | Value.max | Measure
 
-    ds.combined = foreach::foreach(i = 1:nrow(ds.names),
-                                   .combine = rbind) %do%
-    {
-        classifier.performance.original.file.path =
-            replace.strings(c(DATASETS.NAME.PATTERN, CLASSIFIERS.NAME.PATTERN),
-                            c(ds.names[i, 1], ds.names[i, 2]),
-                            CLASSIFIERS.PERFORMANCE.ORIGINAL)
+    data$Model = with(data, factor(Model, levels = rev(levels(Model))))
 
-        readRDS(classifier.performance.original.file.path) %>%
-            mutate(Dataset = ds.names[i, 1], Model = ds.names[i, 2])
-    }
-
-    for (dataset.name in DATASETS.NAMES)
-    {
-        ds.results = ds.combined %>% filter(Dataset == dataset.name)
-
-        for (measure in c("Accuracy", "Sensitivity", "Specificity", "Decisiveness"))
-        {
-            # barplot
-            ds.barplot = ds.results %>% filter(is.na(Missing.attributes)) %>%
-                select(-Missing.attributes, -Dataset) %>%
-                reshape2::melt(id.vars = "Model") %>%
-                filter(variable == measure)  %>%
-                mutate(Model = factor(Model,
-                                      levels = rev(c(CLASSIFIERS.BASELINE, CLASSIFIERS.LIST)),
-                                      ordered = TRUE))
-
-            ggplot(ds.barplot,
-                   aes(x    = Model,
-                       y    = value,
-                       fill = Model)) +
-                geom_bar(stat = "identity") +
-                scale_y_continuous(expand = c(0, 0),
-                                   limits = c(0, 1)) +
-                theme_classic() +
-                geom_hline(yintercept = 0,   color = "grey") +
-                geom_vline(xintercept = 0.4, color = "grey") +
-                theme(axis.title.x    = element_blank(),
-                      axis.title.y    = element_blank(),
-                      axis.text.x     = element_text(color = "grey50"),
-                      axis.text.y     = element_text(color = "black"),
-                      axis.ticks.x    = element_line(color = "grey50"),
-                      axis.ticks.y    = element_line(color = "grey50"),
-                      legend.position = "none",
-                      plot.margin     = unit(c(0.5, 1.5, 0.5, 0.5), "cm")) +
-                coord_flip() +
-                geom_text(aes(x = Model,
-                              y = value,
-                              label = format(value, digits = 3, nsmall = 3)),
-                          hjust  = -0.25,
-                          size   = 3,
-                          colour = "grey45")
-
-            # lineplot
+    ggplot(data,
+           aes(x    = Model,
+               y    = Value.min,
+               fill = Model)) +
+        geom_hline(yintercept = 0.5, color = "grey90", linetype = 2) +
+        geom_hline(yintercept = 1.0, color = "grey90", linetype = 2) +
+        geom_bar(stat  = "identity",
+                 color = "grey60",
+                 width = 0.75) +
+        scale_y_continuous(expand = c(0, 0),
+                           limits = c(0, 1)) +
+        theme_classic() +
+        geom_hline(yintercept = 0,   color = "grey") +
+        geom_vline(xintercept = 0.4, color = "grey") +
+        theme(axis.title.x    = element_blank(),
+              axis.title.y    = element_blank(),
+              axis.text.x     = element_text(color = "grey50"),
+              axis.text.y     = element_text(color = "black"),
+              axis.ticks.x    = element_line(color = "grey50"),
+              axis.ticks.y    = element_line(color = "grey50"),
+              legend.position = "none",
+              plot.margin     = unit(c(0.5, 1.5, 0.5, 0.5), "cm")) +
+        coord_flip() +
+        geom_segment(aes(x = Model, xend = Model,
+                         y = Value.min,
+                         yend = ifelse(!is.na(Value.max), Value.max, Value.min)),
+                     color = "grey60") +
+        geom_segment(aes(x = as.numeric(Model) - 0.25, xend = as.numeric(Model) + 0.25,
+                         y = ifelse(!is.na(Value.max), Value.max, Value.min),
+                         yend = ifelse(!is.na(Value.max), Value.max, Value.min)),
+                     color = "grey60") +
+        geom_text(aes(x = Model,
+                      y = ifelse(!is.na(Value.max), Value.max, Value.min),
+                      label = format(ifelse(!is.na(Value.max), Value.max, Value.min),
+                                     digits = 3, nsmall = 3)),
+                  hjust  = -0.25,
+                  size   = 3,
+                  colour = "grey45") +
+        geom_text(aes(x = Model,
+                      y = ifelse(is.na(Value.max), 1, Value.min),
+                      label = ifelse(is.na(Value.max), "",
+                                     format(Value.min, digits = 3, nsmall = 3))),
+                  hjust  = 1.25,
+                  size   = 3,
+                  colour = "white") +
+        scale_fill_manual(values = palette.fill)
 
 
-            ds.lineplot = ds.results %>% filter(!is.na(Missing.attributes)) %>%
-                select(-Dataset) %>%
-                reshape2::melt(id.vars = c("Model", "Missing.attributes")) %>%
-                filter(variable == measure) %>%
-                filter(!is.na(value) & value > 0) %>%
-                mutate(Model = factor(Model,
-                                      levels = c(CLASSIFIERS.BASELINE, CLASSIFIERS.LIST),
-                                      ordered = TRUE))
-
-            ggplot(ds.lineplot, aes(x = Missing.attributes,
-                                    y     = value,
-                                    fill  = Model,
-                                    color = Model,
-                                    shape = Model,
-                                    order = Model)) +
-                geom_line(size = 1) +
-                geom_point(size = 2, color = "grey30") +
-                scale_x_continuous(expand = c(0, 0),
-                                 limits = c(0, max(ds.lineplot$Missing.attributes)),
-                                 breaks = 0:max(ds.lineplot$Missing.attributes)) +
-                scale_y_continuous(expand = c(0, 0),
-                                   limits = c(0, 1.05)) +
-                theme_classic() +
-                geom_hline(yintercept = 0, color = "grey") +
-                geom_vline(xintercept = 0, color = "grey") +
-                theme(axis.title.x    = element_blank(),
-                      axis.title.y    = element_blank(),
-                      axis.text.x     = element_text(color = "grey50"),
-                      axis.text.y     = element_text(color = "grey50"),
-                      axis.ticks.x    = element_line(color = "grey50"),
-                      axis.ticks.y    = element_line(color = "grey50"),
-                      legend.position = "right",
-                      legend.title    = element_blank(),
-                      plot.margin     = unit(c(0.5, 0, 0.5, 0.5), "cm")) +
-                scale_shape_manual(values = c(21,24,22,25,23,21))
-
-            browser()
-        }
-
-    }
 }
 
-classifiers.original.show.plot()
+get.lineplot = function()
+{
+    # ggplot(ds.lineplot, aes(x = Missing.attributes,
+    #                         y     = value,
+    #                         fill  = Model,
+    #                         color = Model,
+    #                         shape = Model,
+    #                         order = Model)) +
+    #     geom_line(size = 1) +
+    #     geom_point(size = 2, color = "grey30") +
+    #     scale_x_continuous(expand = c(0, 0),
+    #                        limits = c(0, max(ds.lineplot$Missing.attributes)),
+    #                        breaks = 0:max(ds.lineplot$Missing.attributes)) +
+    #     scale_y_continuous(expand = c(0, 0),
+    #                        limits = c(0, 1.05)) +
+    #     theme_classic() +
+    #     geom_hline(yintercept = 0, color = "grey") +
+    #     geom_vline(xintercept = 0, color = "grey") +
+    #     theme(axis.title.x    = element_blank(),
+    #           axis.title.y    = element_blank(),
+    #           axis.text.x     = element_text(color = "grey50"),
+    #           axis.text.y     = element_text(color = "grey50"),
+    #           axis.ticks.x    = element_line(color = "grey50"),
+    #           axis.ticks.y    = element_line(color = "grey50"),
+    #           legend.position = "right",
+    #           legend.title    = element_blank(),
+    #           plot.margin     = unit(c(0.5, 0, 0.5, 0.5), "cm")) +
+    #     scale_shape_manual(values = c(21,24,22,25,23,21))
+}
+
+# ----------------------
+
+df = data.frame(Model = c("Original classifiers", "Uncertaintified classifiers",
+                          "Imputation", "Aggregation strategy"),
+                Value.min = c(0.44, 0.47, 0.62, 0.79),
+                Value.max = c(0.49, 0.55, NA, NA),
+                Measure   = "Accuracy",
+                stringsAsFactors = FALSE)
+df$Model = with(df, factor(Model, levels = Model))
+
+p1 = get.barplot(df)
+print(p1)
